93f5d2e5248871dc348c206912bd326e0bd9db2f
maxkrieg <maxwell.krieg@gmail.com>
2022-03-18 17:24:45 -0600

    save current dc path to file in home dir

--- electron/dockerComposeDir.ts
+++ electron/dockerComposeDir.ts
@@ -1,7 +1,18 @@
-let dockerComposeDir = ''
+import { homedir } from 'os'
+import fs from 'fs'
 
-export const getDockerComposeDir = (): string => dockerComposeDir
+// let dockerComposeDir = ''
+
+export const getDockerComposeDir = (): string => {
+  if (fs.existsSync(`${homedir()}/.docker-compose-dir`)) {
+    const dockerComposeDir = fs.readFileSync(`${homedir()}/.docker-compose-dir`, 'utf8')
+    return dockerComposeDir
+  }
+  console.log('file not found')
+  return ''
+}
 
 export const setDockerComposeDir = (dir: string): void => {
-  dockerComposeDir = dir
+  fs.writeFileSync(`${homedir()}/.docker-compose-dir`, dir)
+  // dockerComposeDir = dir
 }
